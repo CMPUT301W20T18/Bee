8c2686074936dde9f88178b3827b540d6cf41726
yixianLu <43053645+yixianLu@users.noreply.github.com>
2020-03-07 13:37:48 -0700

    update my branch

--- app/src/main/java/com/example/bee/RiderAfterAcceptRequest.java
+++ app/src/main/java/com/example/bee/RiderAfterAcceptRequest.java
@@ -2,28 +2,59 @@ package com.example.bee;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.akexorcist.googledirection.DirectionCallback;
+import com.akexorcist.googledirection.GoogleDirection;
+import com.akexorcist.googledirection.constant.TransportMode;
+import com.akexorcist.googledirection.model.Direction;
+import com.akexorcist.googledirection.model.Info;
+import com.akexorcist.googledirection.model.Leg;
+import com.akexorcist.googledirection.model.Route;
+import com.akexorcist.googledirection.util.DirectionConverter;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class RiderAfterAcceptRequest extends FragmentActivity implements OnMapReadyCallback {
 
@@ -33,6 +64,9 @@ public class RiderAfterAcceptRequest extends FragmentActivity implements OnMapRe
     GoogleMap request_accepted_map;
     private FusedLocationProviderClient client_device;
     TextView driver_name;
+
+    MarkerOptions place1, place2;
+    Boolean drew = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +79,9 @@ public class RiderAfterAcceptRequest extends FragmentActivity implements OnMapRe
             public void onClick(View v) {
                 Intent intent = new Intent(RiderAfterAcceptRequest.this, DriverBasicInformation.class);
                 startActivityForResult(intent, 1);
-
             }
         });
+
 
     }
 
@@ -99,20 +133,123 @@ public class RiderAfterAcceptRequest extends FragmentActivity implements OnMapRe
         Log.d(TAG, "onMapReady: map is ready");
 
         request_accepted_map = googleMap;
-        LatLng Edmonton = new LatLng(53.544388,-113.490929);
-        request_accepted_map.addMarker(new MarkerOptions().position(Edmonton).title("edmonton!"));
-        request_accepted_map.moveCamera(CameraUpdateFactory.newLatLng(Edmonton));
 
-        getDeviceLocation();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+
+//        getDeviceLocation();
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        request_accepted_map.setMyLocationEnabled(true);
+//        request_accepted_map.getUiSettings().setCompassEnabled(true);
+
+        LatLng place1_postion = new LatLng(53.523220,-113.526321);
+        place1 = new MarkerOptions().position(place1_postion).title("Orientation");
+
+//        place1.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+//        request_accepted_map.addMarker(place1);
+//        request_accepted_map.moveCamera(CameraUpdateFactory.newLatLng(place1_postion));
+
+        LatLng place2_postion = new LatLng(53.484300,-113.517250);
+        place2 = new MarkerOptions().position(place2_postion).title("Destination");
+//        place2.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+//        request_accepted_map.addMarker(place2);
+
+        //request_accepted_map.clear();
+        drew = getPoints(place1, place2);
+
+        if (!drew){
+            String text = "Invalid Address";
+            Toast toast = Toast.makeText(RiderAfterAcceptRequest.this, text, Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
         }
-        request_accepted_map.setMyLocationEnabled(true);
-        request_accepted_map.getUiSettings().setCompassEnabled(true);
 
 
     }
+
+
+    private boolean getPoints(MarkerOptions from, MarkerOptions to) {
+        try {
+
+            LatLng from_position = from.getPosition();
+            LatLng to_position = to.getPosition();
+
+            request_accepted_map.addMarker(from
+                    .position(from.getPosition())
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+            request_accepted_map.addMarker(to
+                    .position(to.getPosition())
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
+
+            LatLngBounds latLngBounds = new LatLngBounds.Builder()
+                    .include(from_position)
+                    .include(to_position)
+                    .build();
+            // Move camera to include both points
+            request_accepted_map.setPadding(     0,      350,      0,     0);
+            request_accepted_map.animateCamera(CameraUpdateFactory.newLatLngBounds(latLngBounds, 200));
+            drawRoute(from_position, to_position);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+            return true; }
+
+
+//    // Convert vector drawable to bitmap
+//    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+//        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+//        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+//        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+//        Canvas canvas = new Canvas(bitmap);
+//        vectorDrawable.draw(canvas);
+//        return BitmapDescriptorFactory.fromBitmap(bitmap);
+//    }
+
+    private void drawRoute(LatLng p1, LatLng p2) {
+        GoogleDirection.withServerKey(getString(R.string.web_api_key))
+                .from(p1)
+                .to(p2)
+                .transportMode(TransportMode.DRIVING)
+                .execute(new DirectionCallback() {
+                    @Override
+                    public void onDirectionSuccess(Direction direction) {
+                        if(direction.isOK()) {
+                            Route route = direction.getRouteList().get(0);
+                            Leg leg = route.getLegList().get(0);
+                            ArrayList<LatLng> pointList = leg.getDirectionPoint();
+                            Info distanceInfo = leg.getDistance();
+                            String distance = distanceInfo.getText();
+                            Toast.makeText(RiderAfterAcceptRequest.this, distance, Toast.LENGTH_SHORT).show();
+                            PolylineOptions polylineOptions = DirectionConverter
+                                    .createPolyline(RiderAfterAcceptRequest.this, pointList, 5,
+                                            getResources().getColor(R.color.yellow));
+                            request_accepted_map.addPolyline(polylineOptions);
+                        } else {
+                            //wen ti chu zai zhe li
+                            System.out.println("PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP");
+                            String text = direction.getStatus();
+                            Toast toast = Toast.makeText(RiderAfterAcceptRequest.this, text, Toast.LENGTH_SHORT);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                        }
+                    }
+
+                    @Override
+                    public void onDirectionFailure(Throwable t) {
+                        String text = "Failed to get direction";
+                        Toast toast = Toast.makeText(RiderAfterAcceptRequest.this, text, Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+
+                    }
+                });
+    }
+
+
+
 
 }
