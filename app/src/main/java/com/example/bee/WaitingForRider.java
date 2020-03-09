b9c9af188e9e36e6ebb5076849ceac1318344697
lansing213 <49891102+lansing213@users.noreply.github.com>
2020-03-09 04:50:33 -0600

    Add Waiting for rider class

--- app/src/main/java/com/example/bee/WaitingForRider.java
+++ app/src/main/java/com/example/bee/WaitingForRider.java
@@ -1,11 +1,7 @@
 package com.example.bee;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -13,20 +9,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.akexorcist.googledirection.DirectionCallback;
 import com.akexorcist.googledirection.GoogleDirection;
 import com.akexorcist.googledirection.constant.TransportMode;
 import com.akexorcist.googledirection.model.Direction;
-import com.akexorcist.googledirection.model.Info;
 import com.akexorcist.googledirection.model.Leg;
 import com.akexorcist.googledirection.model.Route;
 import com.akexorcist.googledirection.util.DirectionConverter;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -34,58 +26,56 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import static android.content.ContentValues.TAG;
 
-public class PopUpMap extends FragmentActivity implements OnMapReadyCallback{
-    private FusedLocationProviderClient client_device;
+public class WaitingForRider extends AppCompatActivity implements OnMapReadyCallback {
+    private FirebaseFirestore db;
+    private FirebaseUser user;
+    private String originAddress;
+    private String destAddress;
+    TextView riderName;
+
+    TextView RequestMoneyAmount;
     GoogleMap map;
+
+    RiderDecision riderDecision;
+
+
     MarkerOptions place1;
     MarkerOptions place2;
-    Boolean drew = false;
-    Button AcceptButton;
-    Button CancelButton;
-    private TextView RequestMoneyAmount;
-
+    Button finishButton;
+    double requestAmount;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pop_up_map);
+        setContentView(R.layout.activity_main);
+        RequestMoneyAmount = findViewById(R.id.request_money_amount2);
+        SetMoneyAmount(requestAmount);
         initMap();
-        AcceptButton = findViewById(R.id.accept_button);
-        CancelButton = findViewById(R.id.cancel_button);
-        RequestMoneyAmount = findViewById(R.id.request_money_amount1);
+        riderName = findViewById(R.id.rider_name);
+        finishButton = findViewById(R.id.finish_button);
+        finishButton.setVisibility(View.GONE);
 
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 
-        int width = displayMetrics.widthPixels;
-        int hegiht = displayMetrics.heightPixels;
+        if()
 
-        getWindow().setLayout((int) (width * 0.8), (int) (hegiht * .6));
 
-        AcceptButton.setOnClickListener(new View.OnClickListener() {
+
+
+
+        finishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Intent intent = new Intent(PopUpMap.this, WaitingForRider.class);
-                startActivity(intent);
-            }
-        });
-        CancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final Intent intent = new Intent(PopUpMap.this, WaitingForRider.class);
-                startActivity(intent);
+//                Intent intent = new Intent(WaitingForRider.this, )
+                finish();
             }
         });
 
@@ -94,16 +84,15 @@ public class PopUpMap extends FragmentActivity implements OnMapReadyCallback{
     public void SetMoneyAmount(double MoneyAmount){
         RequestMoneyAmount.setText("$ "+ MoneyAmount);
     }
-
     private void initMap(){
         Log.d(TAG, "Initializing map");
         SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
-        .findFragmentById(R.id.map_pop);
+                .findFragmentById(R.id.map_pop);
 
-        supportMapFragment.getMapAsync((OnMapReadyCallback) PopUpMap.this);
+        supportMapFragment.getMapAsync((OnMapReadyCallback) WaitingForRider.this);
 
     }
-//    @Override
+    @Override
     public void onMapReady(GoogleMap googleMap){
         Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: Map is ready");
@@ -120,7 +109,7 @@ public class PopUpMap extends FragmentActivity implements OnMapReadyCallback{
 
         if (!drew) {
             String text = "Invalid Address";
-            Toast toast = Toast.makeText(PopUpMap.this, text, Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(WaitingForRider.this, text, Toast.LENGTH_SHORT);
             toast.setGravity(Gravity.CENTER, 0, 0);
             toast.show();
 
@@ -179,12 +168,12 @@ public class PopUpMap extends FragmentActivity implements OnMapReadyCallback{
 //                                    String distance = distanceInfo.getText();
 //                                    dist = Double.parseDouble(distance.substring(0, distance.length() - 3));
                             PolylineOptions polylineOptions = DirectionConverter
-                                    .createPolyline(PopUpMap.this, pointList, 5,
+                                    .createPolyline(WaitingForRider.this, pointList, 5,
                                             getResources().getColor(R.color.yellow));
                             map.addPolyline(polylineOptions);
                         } else {
                             String text = direction.getStatus();
-                            Toast toast = Toast.makeText(PopUpMap.this, text, Toast.LENGTH_SHORT);
+                            Toast toast = Toast.makeText(WaitingForRider.this, text, Toast.LENGTH_SHORT);
                             toast.setGravity(Gravity.CENTER, 0, 0);
                             toast.show();
                         }
@@ -193,7 +182,7 @@ public class PopUpMap extends FragmentActivity implements OnMapReadyCallback{
                     @Override
                     public void onDirectionFailure(Throwable t) {
                         String text = "Failed to get direction";
-                        Toast toast = Toast.makeText(PopUpMap.this, text, Toast.LENGTH_SHORT);
+                        Toast toast = Toast.makeText(WaitingForRider.this, text, Toast.LENGTH_SHORT);
                         toast.setGravity(Gravity.CENTER, 0, 0);
                         toast.show();
 
