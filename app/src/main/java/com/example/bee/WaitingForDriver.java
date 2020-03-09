3f2129e2f1399ef454f7fa83d3823721610b078d
Lida Ling <lida1@ualberta.ca>
2020-03-09 14:33:46 -0600

    Added cancel request

--- app/src/main/java/com/example/bee/WaitingForDriver.java
+++ app/src/main/java/com/example/bee/WaitingForDriver.java
@@ -1,12 +1,22 @@
 package com.example.bee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -15,15 +25,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 
 public class WaitingForDriver extends AppCompatActivity {
     private static final String TAG = "TAG";
     private FirebaseUser user;
     private DatabaseReference ref;
     private String userID;
-    private String origin;
-    private String dest;
-    private double cost;
+    private Request request;
     TextView toText;
     TextView fromText;
     TextView costText;
@@ -35,6 +45,7 @@ public class WaitingForDriver extends AppCompatActivity {
         toText = findViewById(R.id.show_to);
         fromText = findViewById(R.id.show_from);
         costText = findViewById(R.id.show_cost);
+        Button cancelRequestBtn = findViewById(R.id.cancel_request);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
         userID = user.getUid();
@@ -42,38 +53,101 @@ public class WaitingForDriver extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         ref = database.getReference("requests").child(userID).child("request");
 
-        displayInfo();
-
-    }
-
-    private void displayInfo() {
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                origin = dataSnapshot.child("origin").getValue(String.class);
-                dest = dataSnapshot.child("dest").getValue(String.class);
-                cost = dataSnapshot.child("cost").getValue(Double.class);
-
-                toText.setText(dest);
-                fromText.setText(origin);
-                costText.setText(String.format("%.2f", cost));
-
-                /*Request request = dataSnapshot.getValue(Request.class);
-                if (request != null) {
+                request = dataSnapshot.getValue(Request.class);
+                if (toText.getText().toString().isEmpty()) {
                     toText.setText(request.getDest());
                     fromText.setText(request.getOrigin());
                     costText.setText(String.format("%.2f", request.getCost()));
-                } else {
-                    Toast.makeText(WaitingForDriver.this, "null", Toast.LENGTH_SHORT).show();
-                }*/
+                }
 
+                if (request != null) {
+                    String driverID = request.getDriverID();
+                    if (driverID != null) {
+                        //insertDriver();
+                        // show dialog to ask to accept ride
+                    }
+                }
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Log.d(TAG, databaseError.toString());
             }
         });
+
+        cancelRequestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toConfirm();
+            }
+        });
     }
 
+    /**
+     * Shows a confirm message that ask the user to confirm their cancel of request
+     */
+    private void toConfirm() {
+        Dialog dialog = new Dialog(WaitingForDriver.this, android.R.style.Theme_Dialog);
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.confirm_cancel_dialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCanceledOnTouchOutside(false);
+        Button confirmBtn = dialog.findViewById(R.id.do_cancel_btn);
+        Button cancelBtn = dialog.findViewById(R.id.not_cancel_btn);
+
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go back to EnterAddressMap activity
+                ref.getParent().removeValue();
+                startActivity(new Intent(WaitingForDriver.this, EnterAddressMap.class));
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void acceptOffer() {
+        request.setStatus(true);
+        HashMap<String,Request> updatedRequest = new HashMap<>();
+        updatedRequest.put("request", request);
+        ref.getParent().setValue(updatedRequest).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "onSuccess: rider accepted the offer");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: " + e.toString());
+            }
+        });
+    }
+
+    private void declineOffer() {
+        request.setDriverID(null);
+        HashMap<String,Request> updatedRequest = new HashMap<>();
+        updatedRequest.put("request", request);
+        ref.getParent().setValue(updatedRequest).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "onSuccess: rider declined the offer");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: " + e.toString());
+            }
+        });
+    }
 
 }
