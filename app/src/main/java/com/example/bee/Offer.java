352b3ec32e268d3a3263223032da5b66443833f7
yongqin1 <yongqin1@ualberta.ca>
2020-03-13 13:48:13 -0600

    java doc update

--- app/src/main/java/com/example/bee/Offer.java
+++ app/src/main/java/com/example/bee/Offer.java
@@ -1,5 +1,9 @@
 package com.example.bee;
 
+/**
+ * This java file is an Object of offer request
+ */
+
 public class Offer {
 //    setup local variables and store them later
     private String startingPoint;
@@ -17,21 +21,49 @@ public class Offer {
     }
 
 //  return offer's starting point
+
+    /**
+     *  Returning starting point on call
+     * @return
+     * return Starting Point for this request
+     */
     String getStartingPoint(){
         return this.startingPoint;
     }
 //  return offer's endPoint
+
+    /**
+     * Returning end point on call
+     * @return
+     * return End Point for this request
+     */
     String getEndPoint(){
         return this.endPoint;
     }
 //  return offer's cost
+
+    /**
+     * Returning request's fare
+     * @return
+     * return current reqeust's fare the rider offered
+     */
     String getFare(){
         return this.fare;
     }
 
+    /**
+     * Returning starting point latitude and longitude
+     * @return
+     * return request's geo location
+     */
     String getLatlng(){
         return this.latlng;
     }
 
+    /**
+     * getRiderID
+     * @return
+     * return rider's Id of current request
+     */
     String getRiderId() {return this.riderId;}
 }
