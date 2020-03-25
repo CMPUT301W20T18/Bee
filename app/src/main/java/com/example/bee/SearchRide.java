bea79a012efa9965feff69b7ab0b039cb9ec0cac
lansing213 <49891102+lansing213@users.noreply.github.com>
2020-03-24 21:56:09 -0600

    Update SearchRide.java

--- app/src/main/java/com/example/bee/SearchRide.java
+++ app/src/main/java/com/example/bee/SearchRide.java
@@ -4,6 +4,7 @@ package com.example.bee;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -98,7 +99,13 @@ public class SearchRide extends AppCompatActivity {
             }
         });
 
-        
+        offerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent show = new Intent(SearchRide.this,PopUpMap.class);
+                startActivity(show);
+            }
+        });
 
 
     }
