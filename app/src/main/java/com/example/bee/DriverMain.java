30591cc3777f1f5f67c92452f8860f86687cdb25
Chuqiao <chuqiao@ualberta.ca>
2020-03-30 20:19:39 -0600

    update driver's menu button

--- app/src/main/java/com/example/bee/DriverMain.java
+++ app/src/main/java/com/example/bee/DriverMain.java
@@ -3,11 +3,16 @@ package com.example.bee;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
@@ -21,6 +26,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  *This is a class that used for Driver, the map with current location
@@ -33,20 +40,26 @@ import com.google.android.gms.tasks.OnSuccessListener;
 // instead of real current location
 
 
-public class DriverMain extends FragmentActivity implements OnMapReadyCallback {
+public class DriverMain extends FragmentActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
 
     GoogleMap map;
     Location currentPos;
     FusedLocationProviderClient fusedLocationProviderClient;
     private static final int REQUEST_CODE = 100;
-
+    private FirebaseAuth firebaseAuth;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.driver_main);
+        setContentView(R.layout.driver_with_drawer);
 
         //mapFragment.getMapAsync(this);
+        ImageView profileButton = findViewById(R.id.profile_btn);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         getCurrentLocation();
 
@@ -62,17 +75,72 @@ public class DriverMain extends FragmentActivity implements OnMapReadyCallback {
             }
         });
 
-        final ImageView profileButton = findViewById(R.id.profile_btn);
-        profileButton.setOnClickListener(new View.OnClickListener(){
 
+//        profileButton.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View v) {
+//                Intent profile = new Intent(DriverMain.this,DrawerActivity.class);
+//                startActivity(profile);
+//            }
+//        });
+        profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent profile = new Intent(DriverMain.this,DrawerActivity.class);
-                startActivity(profile);
+                // Display drawer activity
+                drawerLayout.openDrawer(Gravity.LEFT);
             }
         });
 
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.profile:
+                Toast.makeText(DriverMain.this, "Profile Selected", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DriverMain.this, EditProfileActivity.class));
+                return true;
+            case R.id.rating:
+                Toast.makeText(DriverMain.this, "My Rating Selected", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DriverMain.this, RatingActivity.class));
+                return true;
+            case R.id.history:
+                Toast.makeText(DriverMain.this, "History Selected", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DriverMain.this, ConfirmOfferDialog.class));
+                return true;
+            case R.id.sRider:
+                Toast.makeText(DriverMain.this, "Switch to Rider", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DriverMain.this, EnterAddressMap.class));
+                return true;
+            case R.id.sDriver:
+                Toast.makeText(DriverMain.this, "Switch to Driver", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DriverMain.this, DriverMain.class));
+                return true;
+            case R.id.logout:
+                Toast.makeText(DriverMain.this, "Logout Selected", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(DriverMain.this, LoginActivity.class));
+                firebaseAuth.signOut();
+                finish();
+                return true;
+
+            default:
+                break;
+        }
+        return false;
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
     }
 
 //    This method is used to get current location for use
@@ -135,4 +203,7 @@ public class DriverMain extends FragmentActivity implements OnMapReadyCallback {
             }
         }
     }
+
+
+
 }
