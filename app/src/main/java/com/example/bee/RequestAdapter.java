83b9a2f9f4f33d24d24f8d8433ca6852e868f24f
Chuqiao <chuqiao@ualberta.ca>
2020-04-01 11:43:18 -0600

    update requestAdapter

--- app/src/main/java/com/example/bee/RequestAdapter.java
+++ app/src/main/java/com/example/bee/RequestAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -23,6 +24,17 @@ public class RequestAdapter extends ArrayAdapter<Request> {
             vi = LayoutInflater.from(getContext());
             v = vi.inflate(LAYOUT, null);
         }
+
+        Request request = getItem(position);
+        TextView start, end, price;
+        start = v.findViewById(R.id.start);
+        end = v.findViewById(R.id.end);
+        price = v.findViewById(R.id.price);
+        start.setText(request.getOrigin());
+        end.setText(request.getDest());
+        String cost = String.valueOf(request.getCost());
+        price.setText(cost);
         return v;
     }
+
 }
